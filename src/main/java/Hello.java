b1a69359bec21510eb8ff878be9a5a0adf21fec6
mehmetumuttekagac <mutekagac@gmail.com>
2021-06-29 14:57:46 +0100

    Added more Hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("DEveloping Hello feature");
+        System.out.println("Local Repo is already published to Github");
+        System.out.println("Doing more work");
     }
 }
